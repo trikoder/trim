43ddcdb3070a16db55b538eeb1152c022141c26b
Damir Brekalo <dbrekalo@gmail.com>
2018-10-11 16:57:11 +0200

    Enable setting default items per page via bootData

--- demo/main.js
+++ demo/main.js
@@ -9,6 +9,7 @@ import './scss/main.scss';
 
 app
     .setBootData({
+        itemsPerPage: process.env.NODE_ENV !== 'production' ? 15 : 10,
         usesPushState: process.env.NODE_ENV !== 'production',
         googleMapsApiKey: 'AIzaSyBVqg9EqOqARXVIaKRSC7pJpVeHKDRoU2I',
         baseUrl: process.env.BASE_URL,
--- src/js/components/resourcePagination.vue
+++ src/js/components/resourcePagination.vue
@@ -86,6 +86,7 @@
 import userPreferences from '../library/userPreferences';
 import translate from '../library/translate';
 import {range} from '../library/toolkit';
+import bootData from '../library/bootData';
 
 const Pagination = {
 
@@ -158,7 +159,10 @@ const Pagination = {
 
     getLimit(resourceName = 'default') {
 
-        return userPreferences.get('pagination.' + resourceName + '.itemsPerPage', 15);
+        return userPreferences.get(
+            'pagination.' + resourceName + '.itemsPerPage',
+            bootData('itemsPerPage', 15)
+        );
 
     },
 
