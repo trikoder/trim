2c132c97ba234197a0650a671f92edb8a80d497c
Damir Brekalo <dbrekalo@gmail.com>
2018-10-22 14:47:02 +0200

    Update demo and docs content

--- docs/demo/js/31.demo.38b003441f7aefd3747f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{319:function(e,t,i){"use strict";i.r(t),t.default={extendsController:"BaseResourceEdit",getModel:e=>e.getFromApi({type:"user",id:1}),setupEdit({edit:e}){this.setPageTitle("My settings").addSaveControl(),e.addField("TextFormElement",{label:"Email",name:"email",attributes:{input:{class:"inputType2 size2"}}})}}}}]);
---
+++ docs/demo/js/32.demo.d33e0ee123e3619f993e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{314:function(e,t,i){"use strict";i.r(t),t.default={extendsController:"BaseResourceEdit",getModel:e=>e.getFromApi({type:"user",id:1}),setupEdit({edit:e}){this.setPageTitle("My settings").addSaveControl(),e.addField("TextFormElement",{label:"Email",name:"email",attributes:{input:{class:"inputType2 size2"}}})}}}}]);
