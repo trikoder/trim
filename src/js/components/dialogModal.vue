3a122a0416b73acb6823ed60b8ebde05a69dab99
Damir Brekalo <dbrekalo@gmail.com>
2019-08-04 00:18:46 +0200

    Update modal stacking order

--- src/js/components/dialogModal.vue
+++ src/js/components/dialogModal.vue
@@ -110,7 +110,7 @@ export function confirm(message, onAccept, config) {
 
     .overlay {
 
-        position: fixed; top: 0; bottom: 0; left: 0; right: 0; z-index: 1000;
+        position: fixed; top: 0; bottom: 0; left: 0; right: 0; z-index: 3000;
         background-color: rgba(#000, 0.5); animation: fadeIn 0.5s;
 
     }
