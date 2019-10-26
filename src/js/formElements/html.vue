2c28900a59ee0e5f2138c6c8a472b78a286765ab
Damir Brekalo <dbrekalo@gmail.com>
2019-10-27 00:36:50 +0200

    Update html form element styles

--- src/js/formElements/html.vue
+++ src/js/formElements/html.vue
@@ -118,13 +118,11 @@ export default {
     .htmlInputType1 {
 
         @include fontSans;
-
         font-size: 1.6em; outline: none; margin-bottom: em(30,16);
         min-height: em(22,16); padding: em(10,16) 0;
         border-bottom: 1px solid $colorGrayLight2;
 
         > p, > ul, > ol {
-
             margin-bottom: em(25,16); line-height: 1.4;
             color: $colorGrayDark1;
 
@@ -133,51 +131,48 @@ export default {
                 margin-bottom: 0;
 
             }
-
         }
 
-        > ul {
+        > ul > li {
+            padding-left: em(20,16); position: relative;
+            margin-bottom: em(10,16);
 
-            list-style-type: disc; list-style-position: inside;
-
+            &:before {
+                content: ''; position: absolute;
+                left: 0; top: em(8,16);
+                width: em(5,16); height: em(5,16);
+                background: $colorGrayDark1;
+                border-radius: 50%;
+            }
         }
 
         > ol {
+            list-style-type: decimal;
+        }
 
-            list-style-type: decimal; list-style-position: inside;
-
+        > ol > li {
+            list-style-type: decimal;
+            padding-left: em(15,16); margin-left: em(20,16);
+            margin-bottom: em(10,16);
         }
 
         > h1, > h2, > h3 {
-
-            @include fontSansCondensedBold;
             font-size: em(25,16); margin-bottom: em(25,25); line-height: 1.3;
-
         }
 
-        > h1 { font-size: em(30,16); }
+        > h1 { font-size: em(26,16); }
         > h2 { font-size: em(22,16); }
         > h3 { font-size: em(18,16); }
 
-        > ul, > ol {
-
-            > li {
-
-                list-style: inherit;
-
-            }
-
-        }
-
         a {
-            color: $colorMain1;
+            color: $colorGrayDark1;
+            text-decoration: underline;
+            text-decoration-color: $colorMain1;
         }
 
         &:empty:before {
-
             font-size: 1.6em;
             content: attr(placeholder); display: block; color: #666; pointer-events: none;
-
         }
 
     }
