9d6b0de709350caf678b3ba9353e483b977ecc17
Ivan Nikolić <niksy5@gmail.com>
2023-05-31 13:01:55 +0200

    Update demo and docs

---
+++ docs/assets/index.html-792a2a97.js
@@ -0,0 +1,1 @@
+const a=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"","lang":"en-US","frontmatter":{"layout":"HomeCustom","pageClass":"homeCustom"},"headers":[],"git":{"updatedTime":1539265809000,"contributors":[{"name":"Damir Brekalo","email":"dbrekalo@gmail.com","commits":1}]},"filePathRelative":"README.md"}');export{a as data};
