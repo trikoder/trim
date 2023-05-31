f8ddea271045d482ee94c7b0004bb3f8e272ae78
Ivan Nikolić <niksy5@gmail.com>
2023-05-31 13:01:55 +0200

    Update demo and docs

--- docs/assets/js/4.211443ba.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{174:function(n,t,s){"use strict";s.r(t);var c=s(0),e=Object(c.a)({},(function(){return(0,this._self._c)("div",{staticClass:"content"})}),[],!1,null,null,null);t.default=e.exports}}]);
