3c74cb6644725ced056d44c23768581ea4607049
Ivan Nikolić <niksy5@gmail.com>
2023-06-16 09:50:28 +0200

    Initial setup for Vitepress

---
+++ docs/assets/getting-started.md.0d65af4a.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, O as createStaticVNode } from "./chunks/framework.3d88779c.js";
+const __pageData = JSON.parse('{"title":"Getting started","description":"","frontmatter":{},"headers":[],"relativePath":"getting-started.md","filePath":"getting-started.md"}');
+const _sfc_main = { name: "getting-started.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 11);
+const _hoisted_12 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_12);
+}
+const gettingStarted = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  gettingStarted as default
+};
---
+++ documentation/index.md
@@ -0,0 +1,9 @@
+---
+layout: home
+---
+
+<script setup>
+import HomeCustom from './.vitepress/components/HomeCustom.vue';
+</script>
+
+<HomeCustom />
