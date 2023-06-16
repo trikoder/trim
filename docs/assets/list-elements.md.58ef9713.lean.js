7641010c04ea50ff42b84989e000785764597494
Ivan Nikolić <niksy5@gmail.com>
2023-06-16 09:50:28 +0200

    Initial setup for Vitepress

---
+++ docs/assets/list-elements.md.58ef9713.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, O as createStaticVNode } from "./chunks/framework.3d88779c.js";
+const __pageData = JSON.parse('{"title":"List elements","description":"","frontmatter":{},"headers":[],"relativePath":"list-elements.md","filePath":"list-elements.md"}');
+const _sfc_main = { name: "list-elements.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 55);
+const _hoisted_56 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_56);
+}
+const listElements = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  listElements as default
+};
---
+++ documentation/.vitepress/config.js
@@ -0,0 +1,29 @@
+export default {
+    outDir: '../docs',
+    title: 'Trikoder Trim',
+    description: 'Responsive UI framework for building content management systems with simple authoring api. Designed to run as a browser application connected to json:api powered backend.',
+    base: '/trim/',
+    head: [
+        ['link', {rel: 'shortcut icon', href: `/favicon.ico`}],
+        ['link', {rel: 'apple-touch-icon-precomposed', sizes: '152x152', href: '/apple-touch-icon-152x152.png'}],
+        ['link', {rel: 'icon', sizes: '196x196', href: '/favicon-196x196.png'}]
+    ],
+    themeConfig: {
+        nav: [
+            { text: 'Documentation', link: '/about' },
+            { text: 'Demo', link: 'https://trikoder.github.io/trim/demo/index.html' },
+            { text: 'Github', link: 'https://github.com/trikoder/trim' }
+        ],
+        sidebar: [
+            {text: 'Home', link: '/'},
+            {text: 'About', link: '/about'},
+            {text: 'Getting started', link: '/getting-started'},
+            {text: 'Core concepts and API', link: '/core-concepts-and-api'},
+            {text: 'Adding resource', link: '/adding-resource'},
+            {text: 'Form elements', link: '/form-elements'},
+            {text: 'List elements', link: '/list-elements'},
+            {text: 'Base controllers', link: '/base-controllers'},
+            {text: 'Changelog', link: '/changelog'}
+        ]
+    }
+};
