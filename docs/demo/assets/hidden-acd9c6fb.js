6b65319dfc83837edb225ca9a32af2819eff18f0
Ivan Nikolić <niksy5@gmail.com>
2023-09-28 12:50:17 +0200

    Add documentation

---
+++ docs/demo/assets/hidden-acd9c6fb.js
@@ -0,0 +1,1 @@
+import{_ as n,a5 as s,o as a,b as o,O as d}from"./index-58233bfe.js";const i={elementType:"hidden",mixins:[s],props:{modelValue:{default:""}}},l=["value"];function r(e,u,t,p,c,_){return a(),o("input",d({type:"hidden"},e.inputAttributes,{value:t.modelValue}),null,16,l)}const f=n(i,[["render",r],["__file","hidden.vue"]]);export{f as default};
