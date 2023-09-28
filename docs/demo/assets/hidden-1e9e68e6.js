1a4ef41707192ae6a1e0d3cdd4e9434f3668fb34
Ivan Nikolić <niksy5@gmail.com>
2023-09-28 12:50:17 +0200

    Add documentation

--- docs/demo/assets/hidden-1e9e68e6.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,a5 as s,o as a,b as o,O as d}from"./index-0c1d77fd.js";const i={elementType:"hidden",mixins:[s],props:{modelValue:{default:""}}},l=["value"];function r(e,u,t,p,c,_){return a(),o("input",d({type:"hidden"},e.inputAttributes,{value:t.modelValue}),null,16,l)}const f=n(i,[["render",r],["__file","hidden.vue"]]);export{f as default};
